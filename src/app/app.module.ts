c38e6e7eeb5c2d1d578d4c6a414d34e0e521b995
Memory Clutter <memclutter@gmail.com>
2017-06-10 09:51:35 +0300

    Generate modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ImagesModule } from './modules/images/images.module';
+import { ContainersModule } from './modules/containers/containers.module';
+import { NetworksModule } from './modules/networks/networks.module';
+import { VolumesModule } from './modules/volumes/volumes.module';
+import { SettingsModule } from './modules/settings/settings.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContainersModule,
+    ImagesModule,
+    NetworksModule,
+    SettingsModule,
+    VolumesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
