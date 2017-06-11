b1669558afb412c58477fb35a9971c36c98d61a8
Memory Clutter <memclutter@gmail.com>
2017-06-11 10:07:01 +0300

    Not Found component and router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 import { ContainersModule } from './modules/containers/containers.module';
@@ -12,14 +14,21 @@ import { NetworksModule } from './modules/networks/networks.module';
 import { VolumesModule } from './modules/volumes/volumes.module';
 import { SettingsModule } from './modules/settings/settings.module';
 
+const routes: Routes = [
+  { path: '**', component: NotFoundComponent },
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
+
     ContainersModule,
     DashboardModule,
     ImagesModule,
