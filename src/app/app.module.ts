bc4630ac058ce53163033747ff9daa09b2759eec
Memory Clutter <memclutter@gmail.com>
2017-06-11 13:49:56 +0300

    Implement settings form and settings service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SettingsFormComponent } from './components/settings-form/settings-form.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 import { SidebarService } from './services/sidebar.service';
+import { SettingsService } from './services/settings.service';
+
+import { SidebarMenuItemModel } from './models/sidebar-menu-item.model';
 
 import { ContainersModule } from './modules/containers/containers.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { ImagesModule } from './modules/images/images.module';
 import { NetworksModule } from './modules/networks/networks.module';
 import { VolumesModule } from './modules/volumes/volumes.module';
-import { SettingsModule } from './modules/settings/settings.module';
 
 const routes: Routes = [
+  { path: 'settings', component: SettingsFormComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
@@ -26,20 +31,31 @@ const routes: Routes = [
     HeaderComponent,
     SidebarComponent,
     NotFoundComponent,
+    SettingsFormComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
 
     ContainersModule,
     DashboardModule,
     ImagesModule,
     NetworksModule,
-    SettingsModule,
     VolumesModule,
   ],
-  providers: [SidebarService],
+  providers: [
+    SidebarService,
+    SettingsService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarService.registerMenuItem({
+      title: 'Settings',
+      routerLink: 'settings',
+      order: 50,
+    } as SidebarMenuItemModel);
+  }
 }
---
+++ src/app/models/settings.model.ts
@@ -0,0 +1,5 @@
+export class SettingsModel {
+  dockerHost = 'localhost';
+  dockerPort = 4243;
+  isHttps = false;
+}
