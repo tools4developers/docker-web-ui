1f0c418fc4deccfc68a1001e4b4e8f8971802574
Memory Clutter <memclutter@gmail.com>
2017-06-14 20:13:33 +0300

    List networks service method

---
+++ src/app/modules/networks/models/networks-list-params.model.ts
@@ -0,0 +1,14 @@
+export class NetworksListParams {
+  /**
+   * JSON encoded value of the filters (a map[string][]string) to process on the networks list.
+   * Available filters:
+   *
+   *  - driver=<driver-name> Matches a network's driver.
+   *  - id=<network-id> Matches all or part of a network ID.
+   *  - label=<key> or label=<key>=<value> of a network label.
+   *  - name=<network-name> Matches all or part of a network name.
+   *  - scope=["swarm"|"global"|"local"] Filters networks by scope (swarm, global, or local).
+   *  - type=["custom"|"builtin"] Filters networks by type. The custom keyword returns all user-defined networks.
+   */
+  filters?: string;
+}
---
+++ src/app/modules/networks/services/networks.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { DockerService } from '../../../services/docker.service';
+
+import { NetworksListParams } from '../models/networks-list-params.model';
+import { NetworkModel } from '../models/network.model';
+
+@Injectable()
+export class NetworksService extends DockerService {
+
+  /**
+   * List networks
+   *
+   * @param params
+   */
+  public getNetworks(params?: NetworksListParams): Observable<Array<NetworkModel>> {
+    const url = `${this.getDockerBaseUrl()}networks`;
+    const search = params || {};
+
+    return this.http.get(url, {search})
+      .map((response: Response) => response.json() as Array<NetworkModel>)
+      .catch(this.handleError.bind(this));
+  }
+}
